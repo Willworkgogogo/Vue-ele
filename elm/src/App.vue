0c4f6d6c3f6db667835c6a8acbff78d3e9eb2bba
wangweiwei <will_workhard@163.com>
2017-10-09 19:29:00 +0800

    nav

--- elm/src/App.vue
+++ elm/src/App.vue
@@ -1,11 +1,31 @@
 <template>
   <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评价</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      i'm content
+    </div>
   </div>
+
 </template>
 
 <script>
+  import header from './components/header/header.vue';
 
+  export default {
+    components: {
+      'v-header': header
+    }
+  };
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display: flex
+
 </style>
