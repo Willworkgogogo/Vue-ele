3e9e71034ce9b0ce995f14c5ab0e0658919e997c
wangweiwei <will_workhard@163.com>
2017-10-10 09:56:56 +0800

    router

---
+++ elm/src/components/ratings/ratings.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="ratings">
+    我是rating page
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
