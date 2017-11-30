aab0696c3a1fdbb84ecdff81940f8348eccec5fb
wangweiwei <will_workhard@163.com>
2017-12-01 01:17:11 +0800

    评价页面

--- elm/src/components/ratings/ratings.vue
+++ elm/src/components/ratings/ratings.vue
@@ -1,13 +1,60 @@
 <template>
   <div class="ratings">
-    我是rating page
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .ratings
+    position: absolute
+    top: 176px
+    bottom: 0px
+    left: 0px
+    width: 100%
+    overflow: hidden
+    .overview
+      display: flex
+      padding: 18px 0px
+      .overview-left
+        padding: 6px 0
+        flex: 0 0 137px
+        width: 137px
+        border-right: 1px solid rgba(7, 17, 27, 0.1)
+        text-align: center
+        .score
+          margin-bottom: 6px
+          line-height: 28px
+          font-size: 24px
+          color: rgb(255, 153, 0)
+        .title
+          margin-bottom: 8px
+          font-size: 12px
+          color: rgb(7, 17, 27)
+          line-height: 12px
+        .rank
+          font-size: 10px
+          color: rgb(147, 153, 159)
+          line-height: 10px
+      .overview-right
+        flex: 1
+        padding-left: 24px
 </style>
