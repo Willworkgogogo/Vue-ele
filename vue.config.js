88cda6d92878901ae81186d35b578881c779ca34
Willworkgogogo <will_workhard@163.com>
2019-06-10 11:00:59 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,43 @@
+const ERROR_OK = 0;
+const { seller, goods, ratings } = require('./data.json');
+
 module.exports = {
-  css: {
-    loaderOptions: {
-      stylus: {
-        'resolve url': true,
-        'import': [
-          './src/theme'
-        ]
-      }
-    }
-  },
-  pluginOptions: {
-    'cube-ui': {
-      postCompile: true,
-      theme: true
-    }
-  }
-}
+	css: {
+		loaderOptions: {
+			stylus: {
+				'resolve url': true,
+				import: ['./src/theme']
+			}
+		}
+	},
+	pluginOptions: {
+		'cube-ui': {
+			postCompile: true,
+			theme: true
+		}
+	},
+	devServer: {
+		before(app) {
+			app.get('/api/seller', (req, res) => {
+				res.json({
+					errno: ERROR_OK,
+					data: seller
+				});
+			});
+
+			app.get('/api/goods', (req, res) => {
+				res.json({
+					errno: ERROR_OK,
+					data: goods
+				});
+			});
+
+			app.get('/api/ratings', (req, res) => {
+				res.json({
+					errno: ERROR_OK,
+					data: ratings
+				});
+			});
+		}
+	}
+};
